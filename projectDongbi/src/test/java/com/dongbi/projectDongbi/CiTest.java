8ca1c56578da271cdad9d7cb2df2e604c278e7b4
changheeBong <model_so@naver.com>
2024-08-26 17:46:34 +0900

    test: ci자동화 테스트

---
+++ projectDongbi/src/test/java/com/dongbi/projectDongbi/CiTest.java
@@ -0,0 +1,16 @@
+package com.dongbi.projectDongbi;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CiTest {
+
+    @Test
+    public void ciTest() throws Exception{
+        int n = 2;
+
+        Assertions.assertThat(n).isEqualTo(1);
+    }
+}
