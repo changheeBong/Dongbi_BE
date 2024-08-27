e98443889186fed2775ee9e387dea4112a8401f4
changheeBong <model_so@naver.com>
2024-08-27 10:52:28 +0900

    test: ciTest

--- projectDongbi/src/test/java/com/dongbi/projectDongbi/CiTest.java
+++ projectDongbi/src/test/java/com/dongbi/projectDongbi/CiTest.java
@@ -1,12 +1,14 @@
-package com.dongbi;
+package com.dongbi.projectDongbi;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class CiTest {
 
     @Test
     public void ciTEst() throws Exception{
-        Assertions.assertThat(2).isEqualTo(2);
+        Assertions.assertThat(2).isEqualTo(1);
     }
 }
--- projectDongbi/src/test/java/com/dongbi/projectDongbi/ProjectDongbiApplicationTests.java
+++ projectDongbi/src/test/java/com/dongbi/projectDongbi/ProjectDongbiApplicationTests.java
@@ -1,9 +1,8 @@
 package com.dongbi.projectDongbi;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ProjectDongbiApplicationTests {
 
 	@Test
